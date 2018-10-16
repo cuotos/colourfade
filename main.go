d683d7c039945bf1494b2edba41ffa1e687385f8
Dan Potepa <dan.potepa@lush.co.uk>
2018-10-16 14:45:03 +0100

    use rate limiter instead of sleep

--- main.go
+++ main.go
@@ -8,10 +8,20 @@ import (
 
 var (
 	rgb = [3]uint8{255, 0, 0}
+	increment uint8 = 1
 	interval = time.Millisecond * 10
+	limiter chan struct{}
 )
 
+
 func main() {
+	limiter = make(chan struct{})
+
+	go func() {
+		for range time.NewTicker(interval).C {
+			limiter <- struct{}{}
+		}
+	}()
 
 	dec := 0
 
@@ -19,18 +29,19 @@ func main() {
 		inc := (dec + 1) % 3
 
 		for rgb[inc] < 255 {
-			rgb[inc] = rgb[inc] + 5
+			rgb[inc] = rgb[inc] + increment
+			<-limiter
 			print(rgb)
 		}
 
 		for rgb[dec] > 0 {
-			rgb[dec] = rgb[dec] - 5
+			rgb[dec] = rgb[dec] - increment
+			<-limiter
 			print(rgb)
 		}
 
 		dec = (dec + 1) % 3
 	}
-
 }
 
 func print(rgb [3]uint8) {
@@ -38,5 +49,4 @@ func print(rgb [3]uint8) {
 
 	coloredWord := rgbterm.FgString(s, rgb[0], rgb[1], rgb[2])
 	fmt.Print(coloredWord)
-	time.Sleep(interval)
 }
